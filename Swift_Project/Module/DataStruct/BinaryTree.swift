5ec9f4caecd4c5693536942f091f8401997625ed
ZuckerLui <517207266@qq.com>
2020-09-22 22:55:40 +0800

    初建二叉树

--- Swift_Project/Module/DataStruct/BinaryTree.swift
+++ Swift_Project/Module/DataStruct/BinaryTree.swift
@@ -8,6 +8,42 @@
 
 import UIKit
 
-class BinaryTree: NSObject {
+class BinaryTreeNode {
+    var value: String
+    var left: BinaryTreeNode?
+    var right: BinaryTreeNode?
 
+    init(_ value: String) {
+        self.value = value
+    }
 }
+
+class BinaryTree {
+    var root: BinaryTreeNode?
+    fileprivate var nodes: Array<String>
+    fileprivate var index = -1
+    
+    init(_ items: Array<String>) {
+        self.nodes = items
+        self.root = self.creatTree()
+    }
+    
+    // 以先序二叉树创建二叉树
+    fileprivate func creatTree() -> BinaryTreeNode? {
+        self.index += 1
+        if self.index < self.nodes.count && index >= 0{
+            let item = self.nodes[index]
+            if item == "" {
+                return nil
+            } else {
+                let root = BinaryTreeNode.init(item)
+                root.left = creatTree()
+                root.right = creatTree()
+                return root
+            }
+        }
+        return nil
+    }
+}
+
+
